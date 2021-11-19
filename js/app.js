8ee0da3ed096aba8be5b00bc760b19ce3f04d191
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-19 15:05:40 +0100

    JAVASCRIPT: add random emails generator

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 new Vue({
 	el: "#root",
-	data: {},
-	methods: {},
-	computed: {},
+	data: {
+		emails: [],
+		API_RANDOM_MAIL: "https://flynn.boolean.careers/exercises/api/random/mail",
+	},
+	methods: {
+		generateEmails(len) {
+			Array.from({ length: len }, (_, i) => {
+				return axios.get(this.API_RANDOM_MAIL).then((response) => {
+					this.emails.push(response.data.response);
+				});
+			});
+		},
+	},
+	created() {
+		this.generateEmails(10);
+	},
 });
 
 console.log("js");
